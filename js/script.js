8d67281a556c16ef46e2f6ccb70e3c7bf1492092
Ilya Hancharuk <innot707@gmail.com>
2022-07-31 23:04:31 +0300

    feat: add weather widget

--- js/script.js
+++ js/script.js
@@ -2,13 +2,42 @@ const time = document.querySelector('time')
 const date = document.querySelector('date')
 const greeting = document.querySelector('.greeting')
 const userName = document.querySelector('.name')
+const weatherIcon = document.querySelector('.weather-icon')
+const temperature = document.querySelector('.temperature')
+const weatherDescription = document.querySelector('.weather-description')
+const city = document.querySelector('.city')
 
 
 let dateValue = new Date()
 
+//---------------------weather-----------------------------
 
+city.value = 'Minsk'
+let owfClass
+
+async function getWeather() {  
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=91e0c5938a3b45ed4154661576da4ecf&units=metric`
+    const res = await fetch(url)
+    const data = await res.json()
+    
+    if (res.status === 404) {
+        
+        weatherIcon.classList.remove(owfClass)
+        temperature.textContent = `Error! City ${city.value} is not found.`
+        weatherDescription.textContent = ''
+    } else {
+        owfClass = `owf-${data.weather[0].id}`
+        weatherIcon.classList.add(owfClass)
+        temperature.textContent = `${data.main.temp}°C`
+        weatherDescription.textContent = data.weather[0].description
+    }
+}
+
+getWeather()
+city.addEventListener('change', getWeather)
 
 //----------------------Clocks-----------------------
+
 function showTime() {
     dateValue = new Date()
     let optionsForTime = { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false}    
@@ -19,6 +48,7 @@ function showTime() {
 
     setTimeout(showTime, 1000) 
 }
+
 showTime()
 
 //---------------Greeting-------------------------
@@ -37,20 +67,25 @@ function getTimeOfDay () {
     }   
     setTimeout(getTimeOfDay, 1000)
 }
+
 getTimeOfDay()
+
 greeting.textContent = `Good ${timeOfDay} `
 
 //---------------save user name----------------------------------
 
 function setLocalStorage() {
-    localStorage.setItem('name', userName.value);
+    localStorage.setItem('name', userName.value)
   }
-  window.addEventListener('beforeunload', setLocalStorage)
+window.addEventListener('beforeunload', setLocalStorage)
 
 function getLocalStorage() {
   if(localStorage.getItem('name')) {
-    userName.value = localStorage.getItem('name');
+    userName.value = localStorage.getItem('name')
   }
 }
 
+window.addEventListener('load', getLocalStorage)
+
 //-----------------------------------------------------------
+
